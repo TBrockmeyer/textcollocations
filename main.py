92ec6c2d1c5dda06a16af664b53844e226d0c4e4
TBrockmeyer <t_brockmeyer@gmx.de>
2017-12-06 17:28:34 +0000

    main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ The text should be encoded in UTF-8:
 Try this on a sample text in the resources directory, the German novel "Effie Briest" by Theodor Fontane:
     $ python main.py effie.txt
     
-    
 """
 
 import nltk
